919690004bd1788c27bee4952fb01805c1c1d24d
neox5 <neox5@orion.localdomain>
2025-04-29 22:39:53 +0200

    adds keywords to token list

--- token/token.go
+++ token/token.go
@@ -19,4 +19,39 @@ const (
 	NUMBER     // Numeric sequences (e.g., "123", "42")
 	WHITESPACE // Space (0x20), tab (0x09)
 	NEWLINE    // Line breaks (LF: 0x0A for Unix/Linux, CRLF: 0x0D0A for Windows, CR: 0x0D for classic Mac)
+
+	// Keywords begin
+	keywords_beg
+	KW_INCLUDE // \include
+	KW_BEGIN   // \begin environment
+	KW_END     // \end environment
+	keywords_end
 )
+
+// tokens maps each token to its string representation for debugging
+var tokens = [...]string{
+	ILLEGAL:    "ILLEGAL",
+	EOF:        "EOF",
+	COMMENT:    "COMMENT",
+	COMMAND:    "COMMAND",
+	LBRACE:     "LBRACE",
+	RBRACE:     "RBRACE",
+	LBRACKET:   "LBRACKET",
+	RBRACKET:   "RBRACKET",
+	WORD:       "WORD",
+	NUMBER:     "NUMBER",
+	WHITESPACE: "WHITESPACE",
+	NEWLINE:    "NEWLINE",
+
+	KW_INCLUDE: "\\include",
+	KW_BEGIN:   "\\begin",
+	KW_END:     "\\end",
+}
+
+// String returns the string representation of the token
+func (t Token) String() string {
+	if t < 0 || int(t) >= len(tokens) {
+		return "token(" + string(rune(t)) + ")"
+	}
+	return tokens[t]
+}
